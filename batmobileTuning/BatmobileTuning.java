905279ef496d41dbf2a5b1070f9260a626d4fdd1
Martin Kostadinov <36458675+MartinKostadinov1@users.noreply.github.com>
2019-04-21 19:28:43 +0300

    Create BatmobileTuning.java

---
+++ batmobileTuning/BatmobileTuning.java
@@ -0,0 +1,44 @@
+package KauflandContest2019.batmobileTuning;
+
+import java.util.Scanner;
+
+public class BatmobileTuning {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstLine = scanner.nextLine();
+        if (firstLine.equals("OVER")){
+            return;
+        }
+        String[] line1 = firstLine.split("=|,");
+
+        int x1 = Integer.parseInt(line1[1]);
+        int y1 = Integer.parseInt(line1[3]);
+        int r = Integer.parseInt(line1[5]);
+
+        while(true) {
+
+            String line = scanner.nextLine();
+            if (line.equals("OVER")) {
+                break;
+            }
+            String[] line2 = line.split("=|,");
+
+
+            double x2 = Double.parseDouble(line2[1]);
+            double y2 = Double.parseDouble(line2[3]);
+
+
+            double d = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+
+            if (r - d > 0) {
+                System.out.println("Inside");
+            } else if (r - d == 0) {
+                System.out.println("Border");
+            } else {
+                System.out.println("Outside");
+            }
+        }
+
+    }
+}
