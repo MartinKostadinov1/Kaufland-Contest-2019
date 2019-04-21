9e347abe3e49a5f4dbd6abd311bb55fe59b0af5f
Martin Kostadinov <36458675+MartinKostadinov1@users.noreply.github.com>
2019-04-21 19:30:31 +0300

    Create BatVehicle.java

---
+++ batVehicle/BatVehicle.java
@@ -0,0 +1,26 @@
+package KauflandContest2019.batVehicle;
+
+public abstract class BatVehicle {
+    protected int seats;
+    protected SeatsType seatsType;
+    protected int horsepower;
+    protected double weight;
+    protected Gearbox gearbox;
+    protected Color color;
+    protected PoweredWith poweredWith;
+
+    public BatVehicle(int seats,SeatsType seatsType, int horsepower, double weight, Gearbox gearbox, Color color, PoweredWith poweredWith) {
+        this.seats = seats;
+        this.seatsType = seatsType;
+        this.horsepower = horsepower;
+        this.weight = weight;
+        this.gearbox = gearbox;
+        this.color = color;
+        this.poweredWith = poweredWith;
+    }
+
+    public abstract void accelerate();
+    public abstract void stop();
+
+
+}
