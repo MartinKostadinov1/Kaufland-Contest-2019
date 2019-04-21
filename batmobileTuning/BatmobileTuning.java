35bd63db705c49c97f496830dd4193d393346e9d
Martin Kostadinov <36458675+MartinKostadinov1@users.noreply.github.com>
2019-04-21 19:46:44 +0300

    Update BatmobileTuning.java

--- batmobileTuning/BatmobileTuning.java
+++ batmobileTuning/BatmobileTuning.java
@@ -2,6 +2,11 @@ package KauflandContest2019.batmobileTuning;
 
 import java.util.Scanner;
 
+/**
+ * @author: Martin Kostadinov
+ * @date: 25-03-2019
+ */
+
 public class BatmobileTuning {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
