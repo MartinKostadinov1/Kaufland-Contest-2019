924a019ffe5820f6cc62cc12ab0c0d4c22b66a5b
Martin Kostadinov <36458675+MartinKostadinov1@users.noreply.github.com>
2019-04-21 19:27:33 +0300

    Create BatmanBegins.java

---
+++ batmanBegins/BatmanBegins.java
@@ -0,0 +1,48 @@
+package KauflandContest2019.batmanBegins;
+
+import java.util.Scanner;
+
+public class BatmanBegins {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] lineArgs = line.split(" ");
+
+        int[] totalPeople = new int[lineArgs.length];
+
+        for (int i = 0; i <lineArgs.length ; i++) {
+            totalPeople[i] = Integer.parseInt(lineArgs[i]);
+        }
+
+        int min = totalPeople[0];
+        int max = 0;
+        double avg = 0;
+        int sum = 0;
+
+
+        for (int i = 0; i <totalPeople.length ; i++) {
+            if (totalPeople[i]<=min){
+                min = totalPeople[i];
+            }
+
+            if (totalPeople[i]>max){
+                max = totalPeople[i];
+            }
+
+        }
+        for (int i = 0; i < totalPeople.length; i++) {
+            sum+=totalPeople[i];
+            avg = sum/totalPeople.length;
+        }
+
+
+
+        System.out.printf("Count: %d%n",totalPeople.length);
+        System.out.printf("Min: %d%n",min);
+        System.out.printf("Max: %d%n",max);
+        System.out.printf("Avg: %.2f%n",avg);
+
+        System.out.printf("Sum: %d%n",sum);
+
+    }
+}
