a8597f75001ce9036fce75ff4940d54a4fc1b0bd
Martin Kostadinov <36458675+MartinKostadinov1@users.noreply.github.com>
2019-04-21 19:47:25 +0300

    Update WhySoSerious.java

--- whySoSerious/WhySoSerious.java
+++ whySoSerious/WhySoSerious.java
@@ -2,6 +2,11 @@ package KauflandContest2019.whySoSerious;
 
 import java.util.Scanner;
 
+/**
+ * @author: Martin Kostadinov
+ * @date: 25-03-2019
+ */
+
 public class WhySoSerious {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
