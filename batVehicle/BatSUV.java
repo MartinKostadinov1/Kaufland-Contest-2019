5c35514af594a3f068cb31842e8dca95393df66f
Martin Kostadinov <36458675+MartinKostadinov1@users.noreply.github.com>
2019-04-21 19:44:28 +0300

    Update BatSUV.java

--- batVehicle/BatSUV.java
+++ batVehicle/BatSUV.java
@@ -1,5 +1,10 @@
 package KauflandContest2019.batVehicle;
 
+/**
+ * @author: Martin Kostadinov
+ * @date: 25-03-2019
+ */
+
 public class BatSUV extends BatVehicle {
     public BatSUV() {
         super(7, SeatsType.BASIC, 150, 1850, Gearbox.AUTOMATIC, Color.BLUE, PoweredWith.DIESEL);
